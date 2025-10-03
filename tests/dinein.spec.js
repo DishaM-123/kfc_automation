0c79487cd284a899fc3b661e3c6deaa761111860
Arpana Giri <arpana.giri@crestechsoftware.com>
2025-10-03 12:15:06 +0530

    dineincash

---
+++ tests/dinein.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://in-uat.pwa.kfc.dev/');
+  await page.getByTestId('start-order-button').click();
+  await page.getByTestId('disposition-order-click-handler-Disposition - Dine in').click();
+  await page.getByTestId('store-search-input').click();
+  await page.getByTestId('store-search-input').fill('gach');
+  await page.getByText('Gachibowli, Hyderabad, Telangana, India', { exact: true }).click();
+  await page.getByTestId('searchstore-component').locator('div').filter({ hasText: 'KFC RBD Gachibowli0.9 kmdeliverydine-inPick upGround Floor, Survey No. 124,' }).getByTestId('order-now').click();
+  await page.locator('#category-name-CAT158').getByTestId('category-click-test').click();
+  await page.locator('#category-name-CAT3418').getByTestId('category-click-test').click();
+  await page.getByTestId('add-to-cart-A-35899-43914').click();
+  await page.getByRole('button', { name: 'Close' }).click();
+  await page.getByTestId('navigation-checkout-desktop').click();
+  await page.getByTestId('ptr-children-id').locator('div').filter({ hasText: 'CheckoutSecure CheckoutDINE' }).nth(2).click();
+  await page.getByTestId('continue-as-a-gust').click();
+  await page.locator('body').press('Tab');
+  await page.getByTestId('enter-Full Name-details').fill('arpana');
+  await page.getByTestId('enter-Full Name-details').press('Tab');
+  await page.getByTestId('enter-email-details').fill('agg4740@yum.com');
+  await page.getByTestId('enter-email-details').press('Tab');
+  await page.getByTestId('enter-phoneNumber-details').fill('9473895074');
+  await page.getByTestId('enter-phoneNumber-details').press('Tab');
+  await page.getByTestId('enter- marketingOptIn-details').press('Tab');
+  await page.getByTestId('pay-button').click();
+  await page.locator('body').press('Tab');
+  await page.locator('.payment-option').first().press('Tab');
+  await page.getByTestId('continue-to-payment-btn').click();
+  await page.getByTestId('home-btn').locator('img').click();
+});
